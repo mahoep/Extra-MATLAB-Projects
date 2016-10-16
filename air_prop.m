4ef5602ba8f15dccead06d5c86cf55af6202074e
mahoep <mahoep@users.noreply.github.com>
2016-10-15 22:02:31 -0700

    Update air_prop.m
    This is a matlab function that will output density and pressure of air at a give altitude. Accepts either vectors or scalars.

--- air_prop.m
+++ air_prop.m
@@ -8,7 +8,6 @@ R = 8.31447; % ideal gas constant J/(mol K)
 M = 0.028644; %molar mass of dry air
 
 pressure= p_sealvl*(1-(L*x)/T0).^((g*M)/(R*L)); % in kPa
-
 T = T0-L*x;
 density = (pressure*M)./(R*T).*(1000);
 end
