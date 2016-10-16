2c264267a7db8b0235fd406f4a969b6409d3e2ca
mahoep <mahoep@users.noreply.github.com>
2016-10-15 21:54:45 -0700

    Function file that outpust air properites
    This is a matlab function that will output density and pressure of air at a give altitude. Accepts either vectors or scalars.

---
+++ air_prop.m
@@ -0,0 +1,14 @@
+function  [density,pressure] = air_prop(x)
+
+p_sealvl = 101.325; %sea level pressure in kPa
+T0 = 288.15; % sea level standard temp in K
+g = 9.806665; % gravity constant m/s^2
+L = 0.0065; % K/m  lapse rate
+R = 8.31447; % ideal gas constant J/(mol K)
+M = 0.028644; %molar mass of dry air
+
+pressure= p_sealvl*(1-(L*x)/T0).^((g*M)/(R*L)); % in kPa
+
+T = T0-L*x;
+density = (pressure*M)./(R*T).*(1000);
+end
